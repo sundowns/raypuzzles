a65586dfdba7ea86269024b7c1cf270249d29e7b
Thomas Smallridge <thomas@studentedge.com.au>
2017-10-26 16:22:59 +0800

    Added walls, collision and obstacles. Some jank with high speed collisions clipping into the object too far before calculating their angle. how can we fix dis

--- main.lua
+++ main.lua
@@ -1,55 +1,95 @@
-local world = {}
+local rays = {}
+local obstacles = {}
 local newRayX = nil
 local newRayY = nil
 local tick = 0
+local MAP_WIDTH = 0
+local MAP_HEIGHT = 0
 
 function love.load()
     Vector = require "libs.vector"
     Class = require "libs.class"
+    HC = require "libs.HC"
+    HC.resetHash(20)
     require("src.ray")
     paused = false;
+    MAP_WIDTH = love.graphics.getWidth()
+    MAP_HEIGHT = love.graphics.getHeight()
+    table.insert(obstacles, HC.rectangle(-50, -50, MAP_WIDTH + 100, 50)) --top wall (starts top left)
+    table.insert(obstacles, HC.rectangle(-50, -50, 50, MAP_HEIGHT + 100)) --left wall (starts top left)
+    table.insert(obstacles, HC.rectangle(-50, MAP_HEIGHT, MAP_WIDTH + 100, 50)) -- bottom wall (starts bottom left)
+    table.insert(obstacles, HC.rectangle(MAP_WIDTH, -50, 50, MAP_HEIGHT + 100)) --right wall (starts top right)
+    print("MOUSE1: Place a ray (hold and release for direction/power)")
+    print("MOUSE2: Place a randomly obstacle")
+    print("SPACE: Pause")
+    print("F1: Reset")
 end
 
 function love.update(dt)
     if not paused then
         tick = tick + 1
-        --update each ray!
-        for i, entity in ipairs(world) do
-            entity:updatePosition(dt, tick)
+        for i=#rays,1,-1 do --back to front so we can safely remove (https://stackoverflow.com/questions/12394841/safely-remove-items-from-an-array-table-while-iterating)
+            local entity = rays[i]
+            local kill = entity:update(dt, tick)
+            if kill then
+                table.remove(rays, i)
+            else
+                entity:handleCollisions(dt)
+            end
         end
     end
 end
 
 function love.draw()
-    for i, entity in ipairs(world) do
+    for i, entity in ipairs(rays) do
         entity:draw()
     end
     resetColour()
+    for i, obstacle in ipairs(obstacles) do
+        obstacle:draw('fill')
+    end
     if (newRayX and newRayY) then
-        love.graphics.circle('line', newRayX, newRayY, 3)
+        love.graphics.circle('line', newRayX, newRayY, 4)
     end
 end
 
 function love.keypressed(key, scancode, isrepeat)
     if key == "space" then
         paused = not paused
+    elseif key == "f1" then
+        love.event.quit("restart")
     end
 end
 
 function love.mousepressed(x, y, button, isTouch)
-    if button == 1 then
-        newRayX = x
-        newRayY = y
+    if not paused then
+        if button == 1 then
+            newRayX = x
+            newRayY = y
+        elseif button == 2 then
+            addRandomObstacle(x, y)
+        end
     end
 end
 
 function love.mousereleased(x, y, button, isTouch)
-    if newRayX and newRayY then
-        if (newRayX ~= x or newRayY ~= y) then
-            world[#world+1] = Ray(newRayX, newRayY, x, y)
+    if not paused then
+        if newRayX and newRayY then
+            if (newRayX ~= x or newRayY ~= y) then
+                rays[#rays+1] = Ray(newRayX, newRayY, x, y)
+            end
+            newRayX = nil
+            newRayY = nil
         end
-        newRayX = nil
-        newRayY = nil
+    end
+end
+
+function addRandomObstacle(x, y)
+    local choice = love.math.random(2)
+    if choice == 1 then
+        table.insert(obstacles, HC.rectangle(x, y, love.math.random(50, 100), love.math.random(50, 100)))
+    elseif choice == 2 then
+        table.insert(obstacles, HC.circle(x, y, love.math.random(8,50)))
     end
 end
 
--- src/ray.lua
+++ src/ray.lua
@@ -1,41 +1,65 @@
-local MIN_SPEED = 20
+local MIN_SPEED = 50
+local MAX_SPEED = 350
 local TRAIL_LENGTH = 100
+local PARTICLE_RADIUS = 6
+local LIFESPAN = 30 -- seconds
 
 Ray = Class{
     init = function(self, x, y, atX, atY)
         self.pos = Vector(x, y)
         self.direction = Vector(atX - x, atY - y)
+        self.radius = PARTICLE_RADIUS
+        self.hitbox = HC.circle(x, y, self.radius)
         if (self.direction:len() < MIN_SPEED) then
-            local unitVector = self.direction:normalized()
-            self.direction = unitVector * MIN_SPEED
+            self.direction = self.direction:normalized() * MIN_SPEED
+        elseif (self.direction:len() > MAX_SPEED) then
+            self.direction = self.direction:normalized() * MAX_SPEED
         end
 
         self.red = love.math.random(255)
         self.green = love.math.random(255)
         self.blue = love.math.random(255)
         self.trail = {}
+        self.timetolive = LIFESPAN
     end;
     getColour = function(self)
         return self.red, self.green, self.blue
     end;
-    updatePosition = function(self, dt, tick)
+    update = function(self, dt, tick)
         if (tick % 2 == 0) then
             table.insert(self.trail, self.pos)
         end
 
-        self.pos = self.pos + self.direction * dt
+        self:move(dt, 1)
         if (#self.trail > TRAIL_LENGTH) then
             table.remove(self.trail, 1)
+        end
+
+        self.timetolive = self.timetolive - dt
+        if (self.timetolive < 0) then
+            return true
+        else
+            return false
+        end
+    end;
+    move = function(self, dt, speed_coefficient)
+        self.pos = self.pos + self.direction * speed_coefficient * dt
+        self.hitbox:moveTo(self.pos.x, self.pos.y)
+    end;
+    handleCollisions = function(self, dt)
+        for other, separating_vector in pairs(HC.collisions(self.hitbox)) do
+            self.direction = self.direction + separating_vector
+            self:move(dt, 0.5)
         end
     end;
     draw = function(self)
         local r,g,b = self:getColour()
         love.graphics.setColor(r,g,b,255)
-        love.graphics.circle('fill', self.pos.x, self.pos.y, 3)
+        love.graphics.circle('fill', self.pos.x, self.pos.y, self.radius)
 
         for i = 1, #self.trail do
             love.graphics.setColor(r, g, b, (80/#self.trail)*i)
-            love.graphics.circle("fill", self.trail[i].x, self.trail[i].y, 3)
+            love.graphics.circle("fill", self.trail[i].x, self.trail[i].y, (self.radius/#self.trail)*i) --reduce the size further along!!?
         end
     end;
 }
